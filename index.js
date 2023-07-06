81813c00138cafc9e23cd542875f216f057f2d7a
Dewesh Jha <60105121+DeweshSoc@users.noreply.github.com>
2023-07-06 21:12:24 +0530

    upload

---
+++ index.js
@@ -0,0 +1,6 @@
+
+const div1  = document.getElementById('div1');
+
+div1.addEventListener('click',function (){
+    div1.innerHTML = '1';
+})
---
+++ readme.txt
@@ -0,0 +1,2 @@
+each player gets a number -> 1 or O
+3 * 3 grid
